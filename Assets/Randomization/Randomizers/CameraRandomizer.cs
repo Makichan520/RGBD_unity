e7dd8c9628a6f87202beb0d489fc7189ded4dd09
DarjeeRin <985696345@qq.com>
2021-11-07 12:03:25 +0100

    CameraRandomizer update.

--- Assets/Randomization/Randomizers/CameraRandomizer.cs
+++ Assets/Randomization/Randomizers/CameraRandomizer.cs
@@ -12,20 +12,21 @@ namespace UnityEngine.Perception.Randomization.Randomizers{
     public class CameraRandomizer : Randomizer
     {
         /// <summary>
+        /// The target object of camera, has to be in center of camera's screen.
+        /// </summary>
+        [Tooltip("The target object of camera.")]
+        public GameObject target;
+
+        /// <summary>
         /// The range of random rotations to assign to target objects
         /// </summary>
         [Tooltip("The range of random rotations to assign to target objects.")]
-        public Vector3Parameter rotation = new Vector3Parameter
-        {
-            x = new UniformSampler(0, 360),
-            y = new UniformSampler(0, 360),
-            z = new UniformSampler(0, 360)
-        };
+        public FloatParameter rotation_z = new FloatParameter { value = new UniformSampler(0,360) };
 
         /// <summary>
-        /// The range of random position to assign to target objects
+        /// The range of random relative distance to assign to camera.
         /// </summary>
-        [Tooltip("The range of random rotations to assign to target objects.")]
+        [Tooltip("The range of random relative distance to assign to camera.")]
         public Vector3Parameter position = new Vector3Parameter
         {
             x = new UniformSampler(0, 360),
@@ -34,13 +35,16 @@ namespace UnityEngine.Perception.Randomization.Randomizers{
         };
 
         /// <summary>
-        /// Randomizes the rotation of tagged objects at the start of each scenario iteration
+        /// Randomizes the rotation and position of tagged objects at the start of each scenario iteration
         /// </summary>
         protected override void OnIterationStart()
         {
             var tags = tagManager.Query<CameraRandomizerTag>();
-            foreach (var tag in tags)
-                tag.transform.SetPositionAndRotation(position.Sample(), Quaternion.Euler(rotation.Sample()));
+            foreach (var tag in tags){
+                tag.transform.position = position.Sample();
+                tag.transform.LookAt(target.transform.position);
+                tag.transform.Rotate(0,0,rotation_z.Sample(),Space.Self);
+            }
         }
     }
 }
